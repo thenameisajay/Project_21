53a3eb136485b5b5ee6412dcea4c284a1860fcbb
apm30 <m.ajaypradeep@icloud.com>
2023-08-02 19:36:57 +0100

    name change of the game 21 -> puzzlo.

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="main-container">
-        <h1 className="display-3 fw-bold main-header">XXI</h1>
+        <h1 className="display-3 fw-bold main-header">PUZZLO</h1>
         <Home />
       </div>
     </div>
