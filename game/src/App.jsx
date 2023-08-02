7b4bdd4a952033f225fb159e269b000eb58d8869
apm30 <apm30>
2023-08-02 21:29:33 +0100

    new deploy with sub added.

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <div className="main-container">
         <h1 className="display-3 fw-bold main-header">PUZZLO</h1>
+        <h2 className="display-10 fw-bold main-subheader">-Every Day New Password-</h2>
         <Home />
       </div>
     </div>
