5a05895811551c20615973cf7c0956f2957ccdcd
apm30 <m.ajaypradeep@icloud.com>
2023-08-13 16:06:45 +0100

    streamlined the backend.

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config({ path: '../.env' });
 const express = require("express");
 const leaderboardRoutes = require("./routes/leaderboardRoutes");
 const connectDB = require("./config/db");
